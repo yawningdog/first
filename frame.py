12670fdb58222d8c56ff371b0f27711ee2354ee5
rblalock <yawningdog2002@yahoo.com>
2017-08-05 12:28:36 -0700

    commit

--- frame.py
+++ frame.py
@@ -1,57 +1,24 @@
-import paramiko
-import time
+import wx
 
+app = wx.App()
+win = wx.Frame(None, title="ASA tool", size=(410,335))
+bkg = wx.Panel(win)
 
-def disable_paging(remote_conn):
-    '''Disable paging on a Cisco router'''
+loadButton = wx.Button(bkg, label='Open')
+saveButton = wx.Button(bkg, label='Save')
+filename = wx.TextCtrl(bkg)
+contents = wx.TextCtrl(bkg, style=wx.TE_MULTILINE | wx.HSCROLL)
 
-    remote_conn.send("terminal length 0\n")
-    time.sleep(1)
+hbox = wx.BoxSizer()
+hbox.Add(filename, proportion=1, flag=wx.EXPAND)
+hbox.Add(loadButton, proportion=0, flag=wx.LEFT, border=5)
+hbox.Add(saveButton, proportion=0, flag=wx.LEFT, border=5)
 
-    # Clear the buffer on the screen
-    output = remote_conn.recv(1000)
+vbox = wx.BoxSizer(wx.VERTICAL)
+vbox.Add(hbox, proportion=0, flag=wx.EXPAND | wx.ALL, border=5)
+vbox.Add(contents, proportion=1, flag=wx.EXPAND | wx.LEFT | wx.BOTTOM | wx.RIGHT, border=5)
 
-    return output
+bkg.SetSizer(vbox)
+win.Show()
 
-
-if __name__ == '__main__':
-
-
-    # VARIABLES THAT NEED CHANGED
-    ip = '172.28.5.250'
-    username = 'reblaloc'
-    password = 'Panther@ uncia'
-
-    # Create instance of SSHClient object
-    remote_conn_pre = paramiko.SSHClient()
-
-    # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-    remote_conn_pre.set_missing_host_key_policy(
-         paramiko.AutoAddPolicy())
-
-    # initiate SSH connection
-    remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    print "SSH connection established to %s" % ip
-
-    # Use invoke_shell to establish an 'interactive session'
-    remote_conn = remote_conn_pre.invoke_shell()
-    print "Interactive SSH session established"
-
-    # Strip the initial router prompt
-    output = remote_conn.recv(1000)
-
-    # See what we have
-    print output
-
-    # Turn off paging
-    disable_paging(remote_conn)
-
-    # Now let's try to send the router a command
-    remote_conn.send("\n")
-    remote_conn.send("show int ip brief\n")
-
-    # Wait for the command to complete
-    time.sleep(2)
-    
-    output = remote_conn.recv(5000)
-    print output
+app.MainLoop()
